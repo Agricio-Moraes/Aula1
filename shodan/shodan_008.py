715da08759a89a570caa9206576b0498b4f395ec
yellow <yellow@battosai.yellow.dojo>
2021-09-08 21:14:04 -0400

    shodan 08/09 22:13

---
+++ shodan/shodan_008.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import shodan
+shodan_mykey='6LRqm6zHKbmfeJt5GMHG2JLxjywKD6vv'
+shodan_api = shodan.Shodan(shodan_mykey)
+shodan_item='Microsoft-IIS/8.0'
+shodan_result = shodan_api.search(shodan_item)
+
+
+def shodan_search():
+    print('Buscar por:', shodan_item)
+    print('Numero de ocorrencias:', shodan_result['total'])
+
+shodan_search()
+
